e5971702a7478986cbccf36d1432a313ff0755d7
nia <mangrove0720@outlook.com>
2026-06-07 23:54:17 +0800

    add init.sh

---
+++ init.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+set -e
+
+if [ -n "$(ls -A)" ]; then
+    echo "current directory must be empty."
+    exit 1
+fi
+
+git clone --depth=1 --branch="$1" "https://github.com/boobam22/templates.git" .
+rm -rf .git
+
+PROJECT_NAME=$(basename "$PWD")
+
+find . -type f -name '*.template' | while read -r file; do
+    target="${file%.template}"
+    PROJECT_NAME=$PROJECT_NAME envsubst < "$file" > "$target"
+    rm "$file"
+done
+
+git init
+git add .
+git commit -m "init"
+git remote add origin git@github.com:bobam22/$PROJECT_NAME.git
